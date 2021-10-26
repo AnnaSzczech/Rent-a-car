4b000aa889c8d9d81ddd7760cd72146006df4134
AnnaSzczech <anna.szczech89@gmail.com>
2021-10-27 00:50:10 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,69 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field'; 
+import {MatInputModule} from '@angular/material/input'; 
+import {MatCardModule} from '@angular/material/card';
+import { RegisterComponent } from './register/register.component'; 
+import { RouterModule } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import { ManageReservationsComponent } from './manage-reservations/manage-reservations.component';
+import { CreateCarComponent } from './create-car/create-car.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { DatepickerComponent } from './datepicker/datepicker.component';
+import { AdminUsersComponent } from './admin-users/admin-users.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { UserService } from './user.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    NavbarComponent,
+    ManageReservationsComponent,
+    CreateCarComponent,
+    MainPageComponent,
+    DatepickerComponent,
+    AdminUsersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    NgbModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+    MatTableModule,
+    MatPaginatorModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: "", component: LoginComponent },
+      { path: "register", component: RegisterComponent },
+      { path: "main", component: MainPageComponent },
+      { path: "create-cat", component: CreateCarComponent },
+      { path: "manage", component: ManageReservationsComponent },
+      { path: "users", component: AdminUsersComponent }
+    ])
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/register/register.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { UserService } from '../user.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+  }
+
+  onRegister(form: NgForm) {
+    const email = form.value.email;
+    const pass = form.value.password;
+    this.userService.createUser(email, pass).subscribe(() => console.log('OK'));
+  }
+
+}
